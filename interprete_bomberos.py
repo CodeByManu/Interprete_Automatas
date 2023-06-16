ebd0dbdce0755c1031097df987c9f75b10a3159b
trodriguezam <torotegui@gmail.com>
2023-06-16 11:32:17 -0400

    feat: apariencia de terminal

--- interprete_bomberos.py
+++ interprete_bomberos.py
@@ -1,12 +1,10 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import os
 
 # palabra_reservada: TOKEN
 reserved = {
-    # 'riesgo': 'RIESGO',
-    # 'tranqui': 'TRANQUI',
     'salir': 'EXIT',
-    # ' ': 'WHITESPACE'
 }
 
 """ 
@@ -23,10 +21,7 @@ tokens = [
     ]+ list(reserved.values())
 
 # Expresiones regulares para tokens
-# t_RIESGO = r'riesgo'
-# t_TRANQUI = r'tranqui'
 t_EXIT = r'salir'
-# t_WHITESPACE = r' '
 
 def t_CODE(t):
     r'[0-9][0-9]-[0-9]-[0-9]'
@@ -34,7 +29,6 @@ def t_CODE(t):
     return t
 
 def t_KW(t):
-    # r'(riesgo|tranqui)' #incluir espacio previo?
     r'[a-z]+'
     t.type = reserved.get(t.value, 'KW')
     return t
@@ -48,21 +42,17 @@ def t_error(t):
 
 precedence = (('left', 'CODE', 'KW'),) # necesario?
 
-# pendiente = {}
 respuestas = {
     'B': 'Bomba',
-    'Q': 'Escala',
-    'H': '',
-    'M': 'Mecanica',
-    'R': ''
+    'Q': 'Porta Escala',
+    'H': 'Hazmat',
+    'M': 'Escala',
+    'R': 'Rescate'
 }
 
 def p_respuesta(t):
     'respuesta : s'
-    print(t[1])
-
-# def p_res_es(t):
-#     'respuesta : CODE KW'
+    print(f'AyC {os.getcwd()}> {t[1]}')
 
 def p_res_completa(t):
     's : CODE KW'
@@ -85,66 +75,11 @@ def p_res_completa(t):
         elif t[1] == '10-4-2':
             t[0] = '2R 1B'
 
-# def p_res_code(t):
-#     's : CODE'
-#     t[0] = t[1]
-#     pendiente[t[1]] = ''
-#     print('Especifique tipo para el siguiente codigo ingresado:')
-    
-# def p_res_kw(t):
-#     's : KW'
-#     if pendiente.keys() and t[1] == ('riesgo' or 'tranqui'):
-#         pendiente[pendiente[-1]] = t[1]
-#         t[0] = t[1]
-
-# def p_tipo(t):
-#     'tipo : KW'
-
-
-# def p_respuesta(t):
-#     'respuesta : CODE KW'
-#     if t[3] == 'riesgo':
-#         if t[1] == '10-0-1':
-#             print('3B 2Q 1H')
-#         elif t[1] == '10-0-2':
-#             print('3B 2Q 2M')
-#         elif t[1] == '10-4-1':
-#             print('1B 2R')
-#         elif t[1] == '10-4-2':
-#             print('2B 2R')
-#     elif t[3] == 'tranqui':
-#         if t[1] == '10-0-1':
-#             print('2B 1Q')
-#         elif t[1] == '10-0-2':
-#             print('2B 1Q 1M')
-#         elif t[1] == '10-4-1':
-#             print('1B 1R')
-#         elif t[1] == '10-4-2':
-#             print('2R 1B')
-
 def p_exit(t):
     's : EXIT'
     if t[1] == 'salir':
-        print('terminando el programa...')
+        print(f'AyC {os.getcwd()}> Saliendo del interprete...')
         exit()
-# def p_expr_num(t):
-#     's : N'
-#     t[0]=t[1]
-    
-# def p_expr_id(t):
-#     's : ID'
-#     try:
-#         t[0] = pairs[t[1]]
-#     except LookupError:
-#         print("Variable indefinida '%s'" % t[1])
-#         t[0] = 0
-    
-# def p_expr_op(t):
-#     '''s : CODE KW'''
-#     if t[2] == 'riesgo':
-#         t[0]=t[1]+t[3]
-#     elif t[2] == 'tranqui':
-#         t[0]=t[1]-t[3]
 
 def p_error(t):
     if t is None:
@@ -156,12 +91,12 @@ def p_error(t):
 
 lexer = lex.lex()
 parser = yacc.yacc()
+print(f'AyC {os.getcwd()}> \nGlosario:')
+for pair in respuestas.items():
+    print(f'\t{pair[0]}: {pair[1]}'.expandtabs(4))
 while True:
     try:
-        data = input()
-        # if data == 'salir':
-        #     print('terminando el programa...')
-        #     break
+        data = input(f'AyC {os.getcwd()}> ')
     except EOFError:
         break
     parser.parse(data)
